d9cfaa6acb24656afef5aab475558c54f28a983c
GD3242 <dulguung578@gmail.com>
2022-07-01 22:46:13 +0800

    Initial commit

---
+++ src/main/java/student/Student.java
@@ -0,0 +1,32 @@
+package student;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Student {
+
+    private Long id;
+    private String name;
+    private LocalDate dob;
+    private Integer age;
+    private String email;
+
+
+    public Student(String name, LocalDate dob, Integer age, String email) {
+        this.name = name;
+        this.dob = dob;
+        this.age = age;
+        this.email = email;
+    }
+
+
+}
