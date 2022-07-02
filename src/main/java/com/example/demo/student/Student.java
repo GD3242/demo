d32752172a17df4627c4db2da19d82e5fa128e96
GD3242 <61698354+GD3242@users.noreply.github.com>
2022-07-02 19:47:50 +0800

    added student repository

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,28 +2,52 @@ package com.example.demo.student;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
+@Entity
+@Table
 public class Student {
 
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private LocalDate dob;
+    @Transient
     private Integer age;
     private String email;
 
-
-    public Student(String name, LocalDate dob, Integer age, String email) {
+    public Student(Long id, String name, LocalDate dob, String email) {
+        this.id = id;
         this.name = name;
         this.dob = dob;
-        this.age = age;
         this.email = email;
+    }
+
+    public Student(String name, LocalDate dob, String email) {
+        this.name = name;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
 
